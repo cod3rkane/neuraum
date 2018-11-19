fa4ba4dbd83a1b3d5c1dfd7295852842eea1ba7d
cod3rkane <jcesar.developer@gmail.com>
2018-11-19 10:44:29 +0000

    bugfix

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,22 +1,28 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga';
 
 import { Houses } from './Houses';
 import houseMiddleware from './saga/House';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 const reducers = combineReducers({
-  Houses,
+  Houses
 });
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  ),
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(sagaMiddleware)
+  // other store enhancers if any
 );
+
+const store = createStore(reducers, enhancer);
 
 sagaMiddleware.run(houseMiddleware);
 
